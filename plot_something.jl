3193d2a62ab07625e47b14f2810547d4b28fb13a
thakker.8 <thakker.8@cbe-dnc244508d.coeit.osu.edu>
2021-11-22 13:49:18 -0500

    trial plot

--- plot_something.jl
+++ plot_something.jl
@@ -1,30 +1,12 @@
-using Plots
+using DataFrames
+using VegaLite, VegaDatasets
 using FileIO
-
-@userplot StackedArea
-
-# a simple "recipe" for Plots.jl to get stacked area plots
-# usage: stackedarea(xvector, datamatrix, plotsoptions)
-@recipe function f(pc::StackedArea)
-    x, y = pc.args
-    n = length(x)
-    y = cumsum(y, dims=2)
-    seriestype := :shape
-
-    # create a filled polygon for each item
-    for c=1:size(y,2)
-        sx = vcat(x, reverse(x))
-        sy = vcat(y[:,c], c==1 ? zeros(n) : reverse(y[:,c-1]))
-        @series (sx, sy)
-    end
-end
-
 a = [1,1,1,1.5,2,3]
 b = [0.5,0.6,0.4,0.3,0.3,0.2]
 c = [2,1.8,2.2,3.3,2.5,1.8]
 sNames = ["a","b","c"]
-x = [2001,2002,2003,2004,2005,2006]
+xLabels = [2001,2002,2003,2004,2005,2006]
 
-plotly()
-stackedarea(x, [a b c], labels=reshape(sNames, (1,3)))
+df = DataFrame(year=xLabels, a=a, b=b, c=c)
 
+df |> stack |> @vlplot(:area, x=:year, y={:value, stack=:zero}, color="variable:n") |> save("some_plot")
